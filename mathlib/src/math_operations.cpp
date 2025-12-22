82fec462543a92cb4ed2f16ad650658ee33feb32
azubov <azubovv@yandex.ru>
2025-12-22 12:58:26 +0000

    unit tests added

--- mathlib/src/math_operations.cpp
+++ mathlib/src/math_operations.cpp
@@ -1,5 +1,6 @@
 #include "math_operations.h"
 
+#include <cstdint>
 #include <limits>
 #include <stdexcept>
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "math_operations.h"
 
+#include <cstdint>
 #include <iostream>
 #include <string>
 #include <stdexcept>
