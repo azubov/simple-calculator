3de800b3279d12fa7320d8e6d2880b553c88d1c7
azubov <azubovv@yandex.ru>
2026-02-15 09:58:33 +0000

    refactor: integrate Runner into application startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,6 @@
-#include "Calculator.h"
-#include "Checker.h"
-#include "Parser.h"
-#include "Printer.h"
-
-#include <iostream>
-#include <string>
+#include "Runner.h"
 
 int main(int argc, char* argv[]) {
-    Parser parser;
-    Checker checker;
-    Calculator caclulator;
-    Printer printer;
-
-    try {
-        if (argc > 1) {
-            std::string arg = argv[1];
-            if (arg == "--help") {
-                printer.printHelp();
-                return 0;
-            }
-            throw std::invalid_argument("Unknown argument. Use --help");
-        }
-
-        auto data = parser.parse(std::cin);
-        checker.validate(data);
-        caclulator.calculate(data);
-        printer.printResult(data);
-    } catch (const std::exception& e) {
-        printer.printException(e);
-        return 1;
-    }
-    return 0;
+    Runner runner;
+    return runner.run(argc, argv);
 }
