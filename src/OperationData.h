6af635e486a1b52a79f7793e1bf24efac2e0939e
azubov <azubovv@yandex.ru>
2026-03-25 11:40:08 +0000

    refactor: update OperationData structure for repository integration

--- src/OperationData.h
+++ src/OperationData.h
@@ -3,9 +3,12 @@
 #include <cstdint>
 
 struct OperationData {
+    enum class Status { success, undefined, invalid_argument, overflow_error };
+
     std::int64_t first{};
     std::int64_t second{};
     std::int64_t result{};
     char operation{};
     bool hasSecond{false};
+    Status status{Status::undefined};
 };
