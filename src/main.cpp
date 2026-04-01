310cac15a5311f94b1c20b5f1b5e67e08e87df87
azubov <azubovv@yandex.ru>
2026-04-01 19:53:02 +0300

    refactor: update main entry point to use new calculator pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
-#include "Calculator.h"
-#include "CalculatorCacheRepository.h"
-#include "CalculatorRepository.h"
+#include "CachedCalculatorRepository.h"
 #include "CalculatorService.h"
 #include "Checker.h"
 #include "Log.h"
 #include "PGConnection.h"
 #include "Parser.h"
+#include "PostgresCalculatorRepository.h"
 #include "Printer.h"
 #include "Runner.h"
+#include "SimpleCalculator.h"
+#include "StatementInitializer.h"
 
 #include <fmt/format.h>
 
@@ -18,10 +19,16 @@ int main(int argc, char* argv[]) {
 
     try {
         PGConnection db_connection;
-        CalculatorRepository repository(db_connection);
-        CalculatorCacheRepository cache_repository(repository);
-        Calculator calculator;
-        CalculatorService calculator_service(cache_repository, calculator);
+        StatementInitializer::prepareCalculatorStatements(db_connection);
+
+        PostgresCalculatorRepository pg_repository(db_connection);
+        CachedCalculatorRepository cached_repository(pg_repository);
+        cached_repository.fillCacheFromRepository();
+
+        SimpleCalculator simple_calculator;
+        CalculatorService calculator_service(
+            cached_repository, simple_calculator
+        );
         Runner runner(parser, checker, calculator_service, printer);
 
         return runner.run(argc, argv);
